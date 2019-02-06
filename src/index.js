1c6f530350fa5ce166e633608f61c5713072705c
DainiNimaya <46895695+DainiNimaya@users.noreply.github.com>
2019-02-05 20:29:25 -0800

    posting data through axios and use of interceptors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+
+axios.defaults.baseURL='https://jsonplaceholder.typicode.com';
+
+axios.interceptors.request.use(request=>{
+    console.log(request);
+    return request;
+    },error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(responce=>{
+    console.log(responce);
+    return responce;
+    },error => {
+        console.log(error);
+        return Promise.reject(error)
+    }
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
